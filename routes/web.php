5c0aa1d28a5c78b3837fbbdfec8c024b2c716f2e
Sandy Khaled <120924159+sandykhaled@users.noreply.github.com>
2024-04-22 01:15:28 +0200

    Fortify

--- routes/web.php
+++ routes/web.php
@@ -43,5 +43,5 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+//require __DIR__.'/auth.php';
 require __DIR__ . '/dashboard.php';
